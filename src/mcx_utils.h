a1cb7b10f8cf2bb2ccaaa1e4b39859df353207e7
Qianqian Fang <fangqq@gmail.com>
2009-04-02 18:32:39 +0000

    split the code into multiple units, read/write config files
    git-svn-id: https://svn.code.sf.net/p/mcx/svn/mcextreme_cuda/trunk@19 373d029a-b463-0410-adc7-be649bff2a08

---
+++ src/mcx_utils.h
@@ -0,0 +1,52 @@
+#ifndef _MCEXTREME_UTILITIES_H
+#define _MCEXTREME_UTILITIES_H
+
+#include <stdio.h>
+#include <vector_types.h>
+
+#define MAX_PROP        256
+#define MAX_DETECTORS   256
+#define MAX_PATH_LENGTH 1024
+
+typedef struct MCXMedium{
+	float mua;
+	float mus;
+	float g;
+	float n;
+} Medium;
+
+typedef struct MCXConfig{
+	int nphoton; // this is not used
+	int totalmove;
+	int nthread;
+	int seed;
+	float3 srcpos;
+	float3 srcdir;
+	float tstart;
+	float tstep;
+	float tend;
+	float3 steps;
+	uint3 dim;
+	uint3 crop0;
+	uint3 crop1;
+	int medianum;
+	int detnum;
+	Medium *prop;
+	float4 *detpos;
+	unsigned char *vol;
+	char *session;
+} Config;
+
+void mcx_savedata(float *dat,int len,char *name);
+void mcx_error(int id,char *msg);
+void mcx_loadconfig(FILE *in, Config *cfg);
+void mcx_saveconfig(FILE *in, Config *cfg);
+void mcx_readconfig(char *fname, Config *cfg);
+void mcx_writeconfig(char *fname, Config *cfg);
+void mcx_initcfg(Config *cfg);
+void mcx_clearcfg(Config *cfg);
+void mcx_parsecmd(int argc, char* argv[], Config *cfg);
+void mcx_usage();
+void mcx_loadvolume(char *filename,Config *cfg);
+
+#endif
