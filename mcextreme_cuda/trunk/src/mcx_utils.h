87fc7aeac2425b1c3cf9a875724f7406bd31bff9
fangq <fangq@373d029a-b463-0410-adc7-be649bff2a08>
2009-04-20 18:33:17 +0000

    support multiple repetitions within one executation

--- mcextreme_cuda/trunk/src/mcx_utils.h
+++ mcextreme_cuda/trunk/src/mcx_utils.h
@@ -21,27 +21,34 @@ typedef struct MCXConfig{
 	int totalmove;    /*total move per photon*/
 	int nthread;      /*num of total threads, multiple of 128*/
 	int seed;         /*random number generator seed*/
+	
 	float3 srcpos;    /*src position in mm*/
 	float3 srcdir;    /*src normal direction*/
 	float tstart;     /*start time in second*/
 	float tstep;      /*time step in second*/
 	float tend;       /*end time in second*/
 	float3 steps;     /*voxel sizes along x/y/z in mm*/
+	
 	uint3 dim;        /*domain size*/
 	uint3 crop0;      /*sub-volume for cache*/
 	uint3 crop1;      /*the other end of the caching box*/
 	int medianum;     /*total types of media*/
 	int detnum;       /*total detector numbers*/
-	Medium *prop;     /*optical property mapping table*/
 	float detradius;  /*detector radius*/
+
+	Medium *prop;     /*optical property mapping table*/
 	float4 *detpos;   /*detector positions and radius, overwrite detradius*/
+
+	int maxgate;        /*simultaneous recording gates*/
+	int respin;         /*number of repeatitions*/
+
 	unsigned char *vol; /*pointer to the volume*/
 	char session[MAX_SESSION_LENGTH]; /*session id, a string*/
-	unsigned isrowmajor;/*1 for C-styled array in vol, 0 for matlab-styled array*/
-	int maxgate;        /*simultaneous recording gates*/
+	char isrowmajor;    /*1 for C-styled array in vol, 0 for matlab-styled array*/
 	char isreflect;     /*1 for reflecting photons at boundary,0 for exiting*/
 	char isnormalized;  /*1 to normalize the fluence, 0 for raw fluence*/
 	char issavedet;     /*1 to count all photons hits the detectors*/
+	char issave2pt;     /*1 to save the 2-point distribution, 0 do not save*/
 } Config;
 
 void mcx_savedata(float *dat,int len,Config *cfg);
@@ -53,7 +60,7 @@ void mcx_writeconfig(char *fname, Config *cfg);
 void mcx_initcfg(Config *cfg);
 void mcx_clearcfg(Config *cfg);
 void mcx_parsecmd(int argc, char* argv[], Config *cfg);
-void mcx_usage(void);
+void mcx_usage(char *exename);
 void mcx_loadvolume(char *filename,Config *cfg);
 
 #endif
