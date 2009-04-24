204d9a4395855317795be759f38e2ef9b3b7e5bd
fangq <fangq@373d029a-b463-0410-adc7-be649bff2a08>
2009-04-24 23:46:44 +0000

    minor update

--- mcextreme_cuda/trunk/utils/tddiffusion.m
+++ mcextreme_cuda/trunk/utils/tddiffusion.m
@@ -8,11 +8,11 @@ function Phi = tddiffusion(mua, musp, v, Reff, srcpos,detpos, t)
 % see Boas2002
 
 D = 1/(3*(mua+musp));
-zb = (1+Reff)/(1-Reff)*2*D
+zb = (1+Reff)/(1-Reff)*2*D;
 
 z0 = 1/(musp+mua);
-r=getdistance([srcpos(:,1:2) srcpos(:,3)+z0],detpos)
-r2=getdistance([srcpos(:,1:2) srcpos(:,3)-z0-2*zb],detpos)
+r=getdistance([srcpos(:,1:2) srcpos(:,3)+z0],detpos);
+r2=getdistance([srcpos(:,1:2) srcpos(:,3)-z0-2*zb],detpos);
 
 s=4*D*v*t;
 
