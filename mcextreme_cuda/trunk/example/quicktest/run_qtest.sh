be67804c01fd00837e319a592a596595a62c3c60
fangq <fangq@373d029a-b463-0410-adc7-be649bff2a08>
2009-07-09 22:01:39 +0000

    update examples to use -n input

--- mcextreme_cuda/trunk/example/quicktest/run_qtest.sh
+++ mcextreme_cuda/trunk/example/quicktest/run_qtest.sh
@@ -4,4 +4,6 @@ if [ ! -e semi60x60x60.bin ]; then
   perl -pi -e 's/\x0/\x1/g' semi60x60x60.bin
 fi
 
-time ../../bin/mcextreme -t 1024 -g 10 -m 795590 -f qtest.inp -s qtest -r 1 -a 0 -b 0 -p 18
+#time ../../bin/mcextreme -t 1024 -g 10 -m 795590 -f qtest.inp -s qtest -r 1 -a 0 -b 0 -p 18
+time ../../bin/mcextreme -t 1024 -T 128 -g 10 -n 1000000 -f qtest.inp -s qtest -r 1 -a 0 -b 0
+
