12f06455de54d4eef21c268a24d25621fd94b9a6
Qianqian Fang <fangqq@gmail.com>
2009-10-23 17:39:32 +0000

    renaming mcextreme.cu to .c failed MinGW g++, keep .cu
    git-svn-id: https://svn.code.sf.net/p/mcx/svn/mcextreme_cuda/trunk@105 373d029a-b463-0410-adc7-be649bff2a08

---
+++ src/mcx_core.h
@@ -0,0 +1,17 @@
+#ifndef _MCEXTREME_GPU_LAUNCH_H
+#define _MCEXTREME_GPU_LAUNCH_H
+
+#include "mcx_utils.h"
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+void mcx_run_simulation(Config *cfg);
+int  mcx_set_gpu(int printinfo);
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
