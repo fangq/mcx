b3e85cac9416c7159a7651b86dd48bc0adccd108
Qianqian Fang <fangqq@gmail.com>
2009-12-03 21:53:57 +0000

    update -n to -m for all example scripts
    git-svn-id: https://svn.code.sf.net/p/mcx/svn/mcextreme_cuda/trunk@120 373d029a-b463-0410-adc7-be649bff2a08

--- example/validation/run_validation.sh
+++ example/validation/run_validation.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-time ../../bin/mcx -t 1792 -g 50 -n 596072 -f validation.inp -s semi_infinite -r 12 -a 0 -b 0 -U 1
+time ../../bin/mcx -t 1792 -g 50 -m 596072 -f validation.inp -s semi_infinite -r 12 -a 0 -b 0 -U 1
 #time ../../bin/mcx -t 1024 -g 50 -m 1000000 -f validation_dark.inp -s semi_infinite_dark -r 13 -a 0 -b 0 -U 1
