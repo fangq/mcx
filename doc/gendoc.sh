5842c1d51b36341d3beaccc218b0120ece8ea8fd
Qianqian Fang <fangqq@gmail.com>
2009-12-19 23:16:12 +0000

    update documents, get ready for 0.2 final
    git-svn-id: https://svn.code.sf.net/p/mcx/svn/mcextreme_cuda/trunk@147 373d029a-b463-0410-adc7-be649bff2a08

--- doc/gendoc.sh
+++ doc/gendoc.sh
@@ -4,7 +4,7 @@
 
 ROOTURL="http://mcx.sourceforge.net/cgi-bin/index.cgi?embed=1&keywords"
 
-lynx -dump -width 100 "$ROOTURL=Download" > Download.txt
+lynx -dump "$ROOTURL=Download" > Download.txt
 lynx -dump "$ROOTURL=Doc/Installation" > INSTALL.txt
 lynx -dump "$ROOTURL=Doc/Basics" > Get_Started.txt
 lynx -dump "$ROOTURL=Doc/FAQ" > FAQ.txt
