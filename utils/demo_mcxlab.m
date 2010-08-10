7d130dbdd0203ae2a7b8c1fd575a5479bfc81329
Qianqian Fang <fangqq@gmail.com>
2010-08-10 05:01:00 +0000

    add demo for mcxlab
    git-svn-id: https://svn.code.sf.net/p/mcx/svn/mcextreme_cuda/trunk@211 373d029a-b463-0410-adc7-be649bff2a08

---
+++ utils/demo_mcxlab.m
@@ -0,0 +1,14 @@
+cfg.nphoton=1e7;   
+cfg.vol=uint8(ones(60,60,60));
+cfg.srcpos=[30 30 1];
+cfg.srcdir=[0 0 1];
+cfg.gpuid=1;
+cfg.autopilot=1;
+cfg.prop=[0 0 1 1;0.005 1 1 0];
+cfg.tstart=0;
+cfg.tend=5e-9;
+cfg.tstep=5e-10;
+
+cfg.session='testmcx';
+
+mcxlab(cfg);
