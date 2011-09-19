6f20e2a498467bf8b6ad3641489fd5a85c7e8f5e
fangq <fangq@373d029a-b463-0410-adc7-be649bff2a08>
2011-09-19 21:29:51 +0000

    more tests with detmask, update plotting script

--- mcextreme_cuda/trunk/example/masktest/plotresults.m
+++ mcextreme_cuda/trunk/example/masktest/plotresults.m
@@ -3,10 +3,11 @@ dat=fread(fid,inf,'uchar');
 fclose(fid);
 
 dat=reshape(dat,[8 8 8]);
-pcolor(dat(:,:,1))
+pcolor(dat(:,:,1)')
 
+r=3;
 s=0:pi/20:2*pi;
-c0=[4.2,4.2];
+c0=[4.9,4.1];
 x=c0(1)+r*cos(s);
 y=c0(2)+r*sin(s);
 
