9dc0c24420d39f17e14f0eea1a01c1c1d93765ff
fangq <fangq@373d029a-b463-0410-adc7-be649bff2a08>
2013-02-12 00:00:14 +0000

    restore the accidentally changed script

--- mcextreme_cuda/trunk/example/validation/run_validation.sh
+++ mcextreme_cuda/trunk/example/validation/run_validation.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-#time ../../bin/mcx -t 1792 -g 50 -n 1e8 -f validation.inp -s semi_infinite -r 12 -a 0 -b 0 -U 1
-time ../../bin/mcx -A -n 1e8 -f validation_dark.inp -s semi_infinite_dark -r 1 -a 0 -b 0 -U 1 -R 5
+time ../../bin/mcx -A -g 50 -n 1e8 -f validation.inp -s semi_infinite -a 0 -b 0 -U 1
