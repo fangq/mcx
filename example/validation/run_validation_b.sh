20dc5ed77de2625f90c7c80bb61821b0fc7f232a
Qianqian Fang <fangqq@gmail.com>
2013-02-12 00:00:14 +0000

    restore the accidentally changed script
    git-svn-id: https://svn.code.sf.net/p/mcx/svn/mcextreme_cuda/trunk@303 373d029a-b463-0410-adc7-be649bff2a08

--- example/validation/run_validation_b.sh
+++ example/validation/run_validation_b.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-#time ../../bin/mcx -t 1792 -g 50 -n 1e8 -f validation_b.inp -s semi_infinite_b -r 160 -a 0 -b 1 -B 0 -U 1
-time ../../bin/mcx -A -n 1e8 -f validation_b.inp -s semi_infinite_b -a 0 -b 1 -B 0 -U 1 -R 5
+time ../../bin/mcx -A -g 50 -n 1e8 -f validation_b.inp -s semi_infinite_b -a 0 -b 1 -B 0 -U 1
