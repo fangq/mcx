f1c6e58fa4fd18e2c70c3f47717662354baddf5c
Qianqian Fang <fangqq@gmail.com>
2013-09-26 22:40:03 +0000

    keep ring3 rng up to date, bad property, do not use
    git-svn-id: https://svn.code.sf.net/p/mcx/svn/mcextreme_cuda/trunk@318 373d029a-b463-0410-adc7-be649bff2a08

--- src/logistic_rand_ring3.cu
+++ src/logistic_rand_ring3.cu
@@ -17,18 +17,23 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define RAND_BUF_LEN 3     /*use 5 or 7 will be better*/
-#define R_PI 0.318309886183791f
-#define INIT_LOGISTIC 100
-#define R_MAX_C_RAND (1./RAND_MAX)
+#define MCX_RNG_NAME       "Logistic-Lattice LL3"
+
+#define RAND_BUF_LEN       3        //register arrays
+#define RAND_SEED_LEN      3        //32bit seed length (32*5=160bits)
+#define R_PI               0.318309886183791f
+#define INIT_LOGISTIC      100
+#define INIT_MULT          1812433253      /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
 
 #ifndef DOUBLE_PREC_LOGISTIC
   typedef float RandType;
   #define FUN(x)               (4.f*(x)*(1.f-(x)))
-  #define NU 1e-8f
+  #define NU 1e-7f
   #define NU2 (1.f-2.f*NU)
   #define MIN_INVERSE_LIMIT 1e-7f
   #define logistic_uniform(v)  (acosf(1.f-2.f*(v))*R_PI)
+  #define R_MAX_C_RAND       (1.f/RAND_MAX)
+  #define LOG_MT_MAX         22.1807097779182f
 #else
   typedef double RandType;
   #define FUN(x)               (4.0*(x)*(1.0-(x)))
@@ -36,50 +41,68 @@
   #define NU2 (1.0-2.0*NU)
   #define MIN_INVERSE_LIMIT 1e-12
   #define logistic_uniform(v)  (acos(1.0-2.0*(v))*R_PI)
+  #define R_MAX_C_RAND       (1./RAND_MAX)
+  #define LOG_MT_MAX         22.1807097779182
 #endif
 
 #define RING_FUN(x,y,z)      (NU2*(x)+NU*((y)+(z)))
 
 
-__device__ void logistic_step(RandType *t, RandType *tnew, int len_1){
-/*
-    int i;
-    for(i=0;i<=len_1;i++)
-       t[i]=FUN(t[i]);
-    tnew[0]=RING_FUN(t[0],t[1],t[len_1]);
-    for(i=1;i<len_1;i++)
-       tnew[i]=RING_FUN(t[i],t[i-1],t[i+1]);
-    tnew[len_1]=RING_FUN(t[len_1],t[0],t[len_1-1]);
-*/
-    RandType tmp;
+__device__ void logistic_step(RandType *t, RandType *tnew){
     t[0]=FUN(t[0]);
     t[1]=FUN(t[1]);
     t[2]=FUN(t[2]);
-    tnew[2]=RING_FUN(t[0],t[2],t[1]);
-    tnew[0]=RING_FUN(t[1],t[0],t[2]);
-    tnew[1]=RING_FUN(t[2],t[1],t[0]);
-    tmp=t[0];
-    t[0]=t[1];
-    t[1]=t[2];
-    t[2]=tmp;
+    tnew[0]=RING_FUN(t[0],t[2],t[1]);
+    tnew[1]=RING_FUN(t[1],t[0],t[2]);
+    tnew[2]=RING_FUN(t[2],t[1],t[0]);
 }
 
-__device__ void logistic_rand(RandType *t,RandType *tnew,int len_1){
-    logistic_step(t,tnew,len_1);
-    logistic_step(tnew,t,len_1);
+__device__ void rand_need_more(RandType t[RAND_BUF_LEN],RandType tnew[RAND_BUF_LEN]){
+    for(int i=0;i<10;i++){
+      logistic_step(t,tnew);
+      logistic_step(tnew,t);
+    }
 }
-
-//__device__ RandType logistic_uniform(RandType v){
-//    return acosf(1.0-2.0*v)*R_PI;
-//}
-
+__device__ RandType rand_uniform01(RandType v){
+    return logistic_uniform(v);
+}
 __device__ void logistic_init(RandType *t,RandType *tnew,uint seed[],uint idx){
      int i;
      for(i=0;i<RAND_BUF_LEN;i++)
            t[i]=(RandType)seed[idx*RAND_BUF_LEN+i]*R_MAX_C_RAND;
 
      for(i=0;i<INIT_LOGISTIC;i++)  /*initial randomization*/
-           logistic_rand(t,tnew,RAND_BUF_LEN-1);
+           rand_need_more(t,tnew);
 }
+__device__ void gpu_rng_init(RandType t[RAND_BUF_LEN], RandType tnew[RAND_BUF_LEN],uint *n_seed,int idx){
+    logistic_init(t,tnew,n_seed,idx);
+}
+__device__ void gpu_rng_reseed(RandType t[RAND_BUF_LEN], RandType tnew[RAND_BUF_LEN],uint cpuseed[],uint idx,float reseed){
+    uint *newt=(uint *)(&tnew[0]), seed,i;
+    seed=*((uint *)&reseed);
+    for (i = 0; i<RAND_BUF_LEN; i++){
+        seed = (INIT_MULT * (seed ^ (seed >> 30)) + i) ^ cpuseed[idx*RAND_BUF_LEN+i];
+        newt[i] = seed % RAND_MAX;
+        t[i]=(RandType)(newt[i]*R_MAX_C_RAND);
+    }
+    for(i=0;i<INIT_LOGISTIC;i++)  /*initial randomization*/
+        rand_need_more(t,tnew);
+}
+// generate [0,1] random number for the next scattering length
+__device__ float rand_next_scatlen(RandType t[RAND_BUF_LEN],RandType tnew[RAND_BUF_LEN]){
+    rand_need_more(t,tnew);
+    RandType ran=rand_uniform01(t[1]);
+    if(ran==0.f) ran=rand_uniform01(t[0]);
+    return ((ran==0.f)?LOG_MT_MAX:(-logf(ran)));
+}
+// generate [0,1] random number for the next arimuthal angle
+__device__ float rand_next_aangle(RandType t[RAND_BUF_LEN],RandType tnew[RAND_BUF_LEN]){
+    rand_need_more(t,tnew);
+    return rand_uniform01(t[1]);
+}
+
+#define rand_next_zangle(t1,t2)  rand_next_aangle(t1,t2)
+#define rand_next_reflect(t1,t2) rand_next_aangle(t1,t2)
+#define rand_do_roulette(t1,t2)  rand_next_aangle(t1,t2)
 
 #endif
