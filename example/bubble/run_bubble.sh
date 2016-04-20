452bc065461621c6f587e8f294b07d60c10c8c4e
Qianqian Fang <fangqq@gmail.com>
2016-04-19 20:25:44 -0400

    prefer json shape input over binary input

--- example/bubble/run_bubble.sh
+++ example/bubble/run_bubble.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-if [ ! -e semi60x60x60.bin ]; then
-  dd if=/dev/zero of=semi60x60x60.bin bs=1000 count=216
-  perl -pi -e 's/\x0/\x1/g' semi60x60x60.bin
-fi
 
 time ../../bin/mcx --autopilot 1 \
  --photon 1e6 --input bubble.inp --session bubble0 --repeat 1 \
