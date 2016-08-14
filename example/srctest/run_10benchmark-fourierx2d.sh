78cebbcd6f34a66cf1cbd2b31b3a52bc8381991c
vmgustavo <vm.gustavo31@gmail.com>
2016-08-13 22:57:11 -0400

    Created directory srctest and modified mcx_core.cu formatting

---
+++ example/srctest/run_10benchmark-fourierx2d.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+../../bin/mcx -A -f 10benchmark-fourierx2d.json -b 1 -s 10benchmark-fourierx2d "$@"
---
+++ example/srctest/run_7benchmark-arcsine.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+../../bin/mcx -A -f 7benchmark-arcsine.json -b 1 -s 7benchmark-arcsine "$@"
