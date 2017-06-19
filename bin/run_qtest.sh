be55625f459a5725792238365526d27dbced6586
Qianqian Fang <fangqq@gmail.com>
2017-06-19 12:53:24 -0400

    remove the debug files added by Leiming

--- bin/run_qtest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-../../bin/mcx -A -g 10 -n 1e7 -f qtest.inp -s qtest -r 1 -a 0 -b 0 -G 1 -D P "$@"
-
--- bin/run_widefield.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-../../bin/mcx -A -g 10 -n 1e7 -f qtest_widefield.inp -s qtest_widefield -r 1 -a 0 -b 0 -G 1
