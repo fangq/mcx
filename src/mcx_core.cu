6b55b4b2a3ed113a8b81953465068c378c253e0a
Qianqian Fang <fangqq@gmail.com>
2018-01-26 13:19:57 -0500

    properly scale Jacobian output with voxel size

--- src/mcx_core.cu
+++ src/mcx_core.cu
@@ -2028,9 +2028,8 @@ is more than what your have specified (%d), please use the -H option to specify 
 	       scale=0.f;
 	       for(i=0;i<cfg->nphoton;i++)
 	           scale+=cfg->replay.weight[i];
-	       scale*=cfg->unitinmm;
 	       if(scale>0.f)
-	           scale=1.f/scale;
+	           scale=cfg->unitinmm/scale;
            }
          cfg->normalizer=scale;
 	 cfg->his.normalizer=scale;
