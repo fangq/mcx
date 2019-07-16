80357fcc0a867ad71c51cce141ee2e02ef53d512
Qianqian Fang <fangqq@gmail.com>
2019-07-15 22:17:48 -0400

    remove volumetric mua input in the basic example

--- mcxlab/examples/demo_mcxlab_basic.m
+++ mcxlab/examples/demo_mcxlab_basic.m
@@ -18,11 +18,8 @@ cfg.prop=[0 0 1 1;0.005 1 0 1.37];
 cfg.tstart=0;
 cfg.tend=5e-9;
 cfg.tstep=1e-10;
-% calculate the flux distribution with the given config
 
-cfg.vol=single(0.005*ones(60,60,60));
-cfg.vol(:,:,1:20)=0.1;
-cfg.vol=permute(cfg.vol, [4,1,2,3]);
+% calculate the flux distribution with the given config
 flux=mcxlab(cfg);
 
 
