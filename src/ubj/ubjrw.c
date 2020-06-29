b22025d3c0d4b1047efc2d736e0081243368fc15
Qianqian Fang <fangqq@gmail.com>
2020-06-29 01:06:13 +0100

    update vs project file

--- src/ubj/ubjrw.c
+++ src/ubj/ubjrw.c
@@ -47,10 +47,11 @@ static UBJ_TYPE optimize_type(UBJ_TYPE typein,ubjr_dynamic_t* vals, size_t sz)
 {
 	static const uint32_t intmask = (1 << UBJ_INT8) | (1 << UBJ_UINT8) | (1 << UBJ_INT16) | (1 << UBJ_INT32) | (1 << UBJ_INT64);
 	static const uint32_t stringmask = (1 << UBJ_STRING) | (1 << UBJ_CHAR);
+        uint32_t tm;
 	if (typein != UBJ_MIXED)
 		return typein;
 	//integer optimization can be done here...
-	uint32_t tm = compute_typemask(vals, sz);
+	tm = compute_typemask(vals, sz);
 	if ((tm & intmask) == tm) //if all values are integers
 	{
 		return compute_best_integer_type(vals,sz);	//calculate the optimum type given the data
