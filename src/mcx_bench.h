b6278e7d277098f4474bc0d6ef63cfc66908da5b
Qianqian Fang <fangqq@gmail.com>
2020-08-20 14:24:06 -0400

    add missing new unnit

---
+++ src/mcx_bench.h
@@ -0,0 +1,32 @@
+/***************************************************************************//**
+**  \mainpage Monte Carlo eXtreme - GPU accelerated Monte Carlo Photon Migration
+**
+**  \author Qianqian Fang <q.fang at neu.edu>
+**  \copyright Qianqian Fang, 2009-2018
+**
+**  \section sref Reference:
+**  \li \c (\b Fang2009) Qianqian Fang and David A. Boas, 
+**          <a href="http://www.opticsinfobase.org/abstract.cfm?uri=oe-17-22-20178">
+**          "Monte Carlo Simulation of Photon Migration in 3D Turbid Media Accelerated 
+**          by Graphics Processing Units,"</a> Optics Express, 17(22) 20178-20190 (2009).
+**  \li \c (\b Yu2018) Leiming Yu, Fanny Nina-Paravecino, David Kaeli, and Qianqian Fang,
+**          "Scalable and massively parallel Monte Carlo photon transport
+**           simulations for heterogeneous computing platforms," J. Biomed. Optics, 23(1), 010504, 2018.
+**
+**  \section slicense License
+**          GPL v3, see LICENSE.txt for details
+*******************************************************************************/
+
+/***************************************************************************//**
+\file    mcx_bench.h
+
+@brief   MCX builtin benchmarks
+*******************************************************************************/
+
+#ifndef _MCEXTREME_BENCHMARK_H
+#define _MCEXTREME_BENCHMARK_H
+
+extern const char *benchname[8];
+extern const char *benchjson[7];
+
+#endif
