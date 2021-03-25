dc25a8786a5a5f66708f0693be05f0942200c38b
Qianqian Fang <fangqq@gmail.com>
2021-03-24 21:39:43 -0400

    add mcx cloud service server and client files, partially working

---
+++ mcxone/www/createdb.sql
@@ -0,0 +1,5 @@
+create table mcxcloud(
+time integer primary key, name varchar(32), inst varchar(64), email varchar(64), netname varchar(32), json varchar(1024), shape varchar(1024), jobid varchar(32), status integer, priority float, starttime integer, endtime integer, server varchar(32)
+);
+
+
