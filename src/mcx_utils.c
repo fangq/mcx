168db14d8da231a4e5c73d45c8473cf68d245903
ShijieYan <yanshijie1991@gmail.com>
2021-06-02 18:37:45 -0400

    feat: save Mie function outputs mus, g to a file

--- src/mcx_utils.c
+++ src/mcx_utils.c
@@ -1363,6 +1363,8 @@ void mcx_prep_polarized(Config *cfg){
     
     cfg->smatrix=(float4 *)malloc(cfg->polmedianum*NANGLES*sizeof(float4));
     POLMedium *polprop=cfg->polprop;
+    FILE *pfile;
+    pfile=fopen("Mie_outputs.dat","w");
     for(int i=0;i<cfg->polmedianum;i++){
         prop[i+1].mua=polprop[i].mua;
         prop[i+1].n=polprop[i].nmed;
@@ -1377,9 +1379,18 @@ void mcx_prep_polarized(Config *cfg){
         
         /* compute scattering coefficient (in mm^-1) */
         prop[i+1].mus=qsca*A*polprop[i].rho*1e3;
+
+        /* save Mie function outputs to a file */
+        if(pfile!=NULL){
+            fprintf(pfile,"Tissue type %d: ",i+1);
+            fprintf(pfile,"mua=%5.5f mm^(-1), radius=%5.5f micron, rho=%5.5f micron^(-3), n_sph=%5.5f, n_med=%5.5f\n",
+                polprop[i].mua,polprop[i].r,polprop[i].rho,polprop[i].nsph,polprop[i].nmed);
+            fprintf(pfile,"Mie function outputs: mus=%5.5f mm^(-1), g=%5.5f\n\n",prop[i+1].mus,g);
+        }
     }
     cfg->prop=prop;
     free(mu);
+    if(pfile) fclose(pfile);
 }
 
 /**
