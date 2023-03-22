291adf54a2ffc7c8969629d2655bb8f31f045073
Ernesto Pini <pinie@lens.unifi.it>
2023-03-22 15:13:15 +0100

    allow mus=0, avoid unnecessary casting of scalars to double

--- src/mcx_core.cu
+++ src/mcx_core.cu
@@ -1935,7 +1935,7 @@ __global__ void mcx_main_loop(uint media[], OutputType field[], float genergy[],
                 if (gcfg->outputtype == otEnergy) {
                     weight = w0 - p.w;
                 } else if (gcfg->outputtype == otFluence || gcfg->outputtype == otFlux) {
-                    weight = (prop.mua/prop.mus < 0.001) ? (w0 * len * (1 - prop.mua * len / 2)) : ((w0 - p.w) / (prop.mua));
+                    weight = (prop.mua * len < 0.001f) ? (w0 * len * (1.f - prop.mua * len * 0.5f)) : ((w0 - p.w) / (prop.mua));
                 } else if (gcfg->seed == SEED_FROM_FILE) {
                     if (gcfg->outputtype == otJacobian || gcfg->outputtype == otRF) {
                         weight = replayweight[(idx * gcfg->threadphoton + min(idx, gcfg->oddphotons - 1) + (int)f.ndone)] * f.pathlen;
