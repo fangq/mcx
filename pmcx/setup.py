53e2681793e8448432b9568816236690f1d23b7d
Qianqian Fang <fangqq@gmail.com>
2023-09-05 00:11:31 -0400

    update pmcx after fixing the regression due to #164

--- pmcx/setup.py
+++ pmcx/setup.py
@@ -119,7 +119,7 @@ class CMakeBuild(build_ext):
 setup(
     name="pmcx",
     packages=['pmcx'],
-    version="0.1.1",
+    version="0.1.2",
     requires=['numpy'],
     license='GPLv3+',
     author="Matin Raayai Ardakani, Qianqian Fang, Fan-Yu Yen",
