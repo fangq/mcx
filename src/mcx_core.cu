fangq/mcx

commit 3067a26d8bc333edae105cb3bf9eb84c2fd22ae0

fix incorrect handling of near-zero mus, fix #174, fix test

src/mcx_core.cu
@@ -1838,7 +1838,7 @@ __global__ void mcx_main_loop(uint media[], OutputType field[], float genergy[],
         slen = fmin(slen, f.pscat);
 
         /** final length that the photon moves - either the length to move to the next voxel, or the remaining scattering length */
-        len = slen / (fmax(prop.mus, EPS) * (v.nscat + 1.f > gcfg->gscatter ? (1.f - prop.g) : 1.f));
+        len = ((prop.mus == 0.f) ? len : (slen / prop.mus * (v.nscat + 1.f > gcfg->gscatter ? (1.f - prop.g) : 1.f)));
 
         /** perform ray-interface intersection test to consider intra-voxel curvature (SVMC mode) */
         if (issvmc) {

test/testmcx.sh
@@ -111,7 +111,7 @@ if [ -z "$temp" ]; then echo "fail to save photon seeds"; fail=$((fail+1)); else
 
 echo "test photon replay flag -E ... "
 rm -rf replaytest.*
-temp=`($MCX --bench cube60 -s replaytest -q 1 -S 0 $PARAM && $MCX --bench cube60 -E replaytest.mch -S 0 $PARAM) | sed 's/\x1b\[[0-9;]*m//g' | grep -o -E 'detected.*[0-9.]+ photons' | sort | uniq -c | grep '^\s*2\s*detected'`
+temp=`($MCX --bench cube60 -s replaytest -q 1 -S 0 $PARAM && $MCX --bench cube60 -E replaytest.mch -S 0 $PARAM) | sed 's/\x1b\[[0-9;]*m//g' | grep -o -E 'detected\s+[0-9]+ photons' | sort | uniq -c | grep '^\s*2\s*detected'`
 if [ -z "$temp" ]; then echo "fail to run photon replay -E"; fail=$((fail+1)); else echo "ok"; fi
 
 echo "test photon replay ... "

commit 4d9cf86f37431e67c5ce292d33e4f58b6b0351de

fix critical bug introduced by 3067a26 for #174,fix 2d demo

src/mcx_core.cu
@@ -1863,7 +1863,7 @@ __global__ void mcx_main_loop(uint media[], OutputType field[], float genergy[],
         slen = fminf(slen, f.pscat);
 
         /** final length that the photon moves - either the length to move to the next voxel, or the remaining scattering length */
-        len = ((prop.mus == 0.f) ? len : (slen / prop.mus * (v.nscat + 1.f > gcfg->gscatter ? (1.f - prop.g) : 1.f)));
+        len = ((prop.mus == 0.f) ? len : (slen / (prop.mus * (v.nscat + 1.f > gcfg->gscatter ? (1.f - prop.g) : 1.f))));
 
         /** perform ray-interface intersection test to consider intra-voxel curvature (SVMC mode) */
         if (issvmc) {