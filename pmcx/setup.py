28e510130e6075b7a1b68b45f77973a154437a7d
Qianqian Fang <fangqq@gmail.com>
2023-11-27 22:15:54 -0500

    [pmcx] bump pmcx version to 0.2.7

--- pmcx/setup.py
+++ pmcx/setup.py
@@ -123,7 +123,7 @@ class CMakeBuild(build_ext):
 setup(
     name="pmcx",
     packages=["pmcx"],
-    version="0.2.6",
+    version="0.2.7",
     requires=["numpy"],
     license="GPLv3+",
     author="Matin Raayai Ardakani, Qianqian Fang, Fan-Yu Yen",
