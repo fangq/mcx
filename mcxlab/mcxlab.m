86dcba986cf8b0da2aba1a809592d391a95c5479
Qianqian Fang <fangqq@gmail.com>
2024-12-04 18:25:21 -0500

    [bug] force most scalar inputs to be double to avoid incorrect typecasting

--- mcxlab/mcxlab.m
+++ mcxlab/mcxlab.m
@@ -449,7 +449,12 @@ end
 
 if (isstruct(varargin{1}))
     for i = 1:length(varargin{1})
-        castlist = {'srcpattern', 'srcpos', 'detpos', 'prop', 'workload', 'srcdir', 'srciquv'};
+        castlist = {'srcpattern', 'srcpos', 'detpos', 'prop', 'workload', 'srcdir', 'srciquv', 'isnormalized'
+                    'isreflect', 'nphoton', 'nblocksize', 'nthread', 'tstart', 'tend', 'maxdetphoton', 'maxgate'
+                    'respin', 'isref3', 'isrefint', 'isgpuinfo', 'issrcfrom0', 'autopilot', 'minenergy', 'unitinmm'
+                    'voidtime', 'issavedet', 'issaveseed', 'issaveref', 'issaveexit', 'ismomentum', 'isspecular'
+                    'replaydet', 'maxvoidstep', 'maxjumpdebug', 'gscatter', 'srcnum', 'srcid', 'omega', 'issave2pt'
+                    'lamda', 'steps', 'crop0', 'crop1'};
         for j = 1:length(castlist)
             if (isfield(varargin{1}(i), castlist{j}))
                 varargin{1}(i).(castlist{j}) = double(varargin{1}(i).(castlist{j}));
