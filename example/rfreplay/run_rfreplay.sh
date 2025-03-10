2eacfb821ccfcb12cf721f842ecf56510b1329d3
Qianqian Fang <fangqq@gmail.com>
2025-03-10 18:17:06 -0400

    [example] fix rf replay script to use .jdat output, fix #239

--- example/rfreplay/run_rfreplay.sh
+++ example/rfreplay/run_rfreplay.sh
@@ -4,4 +4,4 @@
 ../../bin/mcx -f baseline.json -q 1 "$@"
 
 # then run RF replay to get RF Jacobian, output data has 6 dimensions
-../../bin/mcx -f rfreplay.json -E baseline.mch -d 0 -F jnii "$@"
+../../bin/mcx -f rfreplay.json -E baseline_detp.jdat -d 0 -F jnii "$@"
