d802d7817142cb92de22d072f9ef28c4e56df01b
Qianqian Fang <fangqq@gmail.com>
2026-03-31 23:02:40 -0400

    [feat] make mcxplotphotons support traj output data

--- utils/mcxplotphotons.m
+++ utils/mcxplotphotons.m
@@ -10,8 +10,16 @@ function varargout = mcxplotphotons(traj, varargin)
 %    author: Qianqian Fang (q.fang <at> neu.edu)
 %
 %    input:
-%        traj: the 5th output of mcxlab, storing the photon trajectory info
-%           traj.id: the photon index being recorded
+%        traj: the photon trajectory data, which can be one of the following:
+%           (1) mcxlab 5th output struct with traj.id/traj.pos/traj.data fields
+%           (2) a struct with photonid/p/w0 fields (mmc/rtmmc-vulkan _traj.jdat
+%               Trajectory object), where photonid is 0-based; rtmmc-vulkan
+%               also stores event type: 0=launch, 1=scatter, 2=terminate,
+%               3=boundary crossing (refraction/reflection)
+%           (3) a struct with a MCXData.Trajectory sub-struct (the full
+%               _traj.jdat file loaded directly, e.g. via loadjson)
+%           (4) an Nx6 numeric matrix [id,x,y,z,weight,reserved]
+%           traj.id: the photon index being recorded (0-based)
 %           traj.pos: the 3D position of the photon; for each photon, the
 %                  positions are stored in serial order
 %           traj.data: the combined output, in the form of
@@ -27,6 +35,13 @@ function varargout = mcxplotphotons(traj, varargin)
 %    License: GPLv3, see https://mcx.space for details
 %
 
+if (isstruct(traj) && isfield(traj, 'MCXData') && isfield(traj.MCXData, 'Trajectory'))
+    traj = traj.MCXData.Trajectory;
+end
+if (isstruct(traj) && isfield(traj, 'photonid') && isfield(traj, 'p') && isfield(traj, 'w0'))
+    traj = struct('id', traj.photonid(:), 'pos', traj.p, 'weight', traj.w0(:), ...
+                  'data', [typecast(uint32(traj.photonid(:)'), 'single'); traj.p'; traj.w0(:)'; zeros(1, numel(traj.w0))]);
+end
 if (isstruct(traj) && ~isfield(traj, 'id') && isfield(traj, 'data'))
     traj = struct('id', typecast(single(traj.data(1, :)'), 'uint32'), 'pos', traj.data(2:4, :)', 'weight', traj.data(5, :)', 'data', traj.data);
 end
@@ -39,7 +54,7 @@ lineend = (diff(newid) > 0);
 newidx = cumsum([0; lineend] + 1);
 newpos = nan(length(idx) + length(lineend), 4);
 newpos(newidx, 1:3) = traj.pos(idx, :);
-newpos(newidx, 4) = traj.data(5, :)';
+newpos(newidx, 4) = traj.data(5, idx)';
 
 if (~(length(varargin) == 1 && strcmp(varargin{1}, 'noplot')))
     edgealpha = 1 - (1 - (exist('OCTAVE_VERSION', 'builtin') ~= 0)) * 0.75;  % octave 6 returns empty if edgealpha<1
